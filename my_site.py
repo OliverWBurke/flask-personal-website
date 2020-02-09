979e20bbcf55319d8bfc383b23f5b921e9514f29
Oliver Burke <oliverwburke@gmail.com>
2020-02-09 21:41:58 +0000

    add requirements and black hooks
    add requirements
    add requirements-dev
    add pre-commit for black
    run through black

--- my_site.py
+++ my_site.py
@@ -3,9 +3,9 @@ import json
 import requests
 import os
 import pathlib
+
 file_path = pathlib.Path(__file__).parent.absolute()
 app = Flask(__name__)
-
 
 
 @app.route("/")
@@ -13,38 +13,41 @@ app = Flask(__name__)
 def home():
     page = "Home"
     body_text = "Not much content here yet!"
-    return render_template('home.html',
-                            page_title = page,
-                            page_body_text = body_text)
+    return render_template("home.html", page_title=page, page_body_text=body_text)
+
 
 @app.route("/timeline")
 def timeline():
     page = "Timeline"
-    with open (os.path.join(file_path,'static/page_data/timeline.json')) as timeline_file:
+    with open(
+        os.path.join(file_path, "static/page_data/timeline.json")
+    ) as timeline_file:
         timeline = json.load(timeline_file)
     timeline = add_descriptions(timeline)
-    return render_template('timeline.html',
-                            page_title = page,
-                            timeline = timeline)
+    return render_template("timeline.html", page_title=page, timeline=timeline)
+
 
 @app.route("/projects")
 def projects():
     page = "Projects"
     body_text = "Not much content here yet!"
-    return render_template('home.html',
-                            page_title = page,
-                            page_body_text = body_text)
+    return render_template("home.html", page_title=page, page_body_text=body_text)
+
 
 def add_descriptions(file):
-    for company,company_details in file.items():
-        for role,role_details in company_details["roles"].items():
-            if role_details["role_description"] =="":
+    for company, company_details in file.items():
+        for role, role_details in company_details["roles"].items():
+            if role_details["role_description"] == "":
                 role_details["role_description"] = get_filler()
     return file
 
-def get_filler(paragraphs=1, length='short'):
-    text = requests.get('https://loripsum.net/api/{}/{}/plaintext'.format(paragraphs, length))
+
+def get_filler(paragraphs=1, length="short"):
+    text = requests.get(
+        "https://loripsum.net/api/{}/{}/plaintext".format(paragraphs, length)
+    )
     return text.text
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
